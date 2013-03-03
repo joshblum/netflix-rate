d8a31a14b72071957db4be40a3406fd9a1d77b02
Joshua Blum <joshblum@mit.edu>
2013-03-02 23:59:50 -0500

    start

--- app.py
+++ app.py
@@ -2,16 +2,24 @@ import env
 import flask
 import os
 
+from flask import render_template
+
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    
+    return render_template('home.html')
 
-    return flask.Response(**res)
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return render_template('500.html'), 500
 
 if __name__ == "__main__":
     app.config.from_pyfile("env.py")
     host = "0.0.0.0"
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 7070))
     app.run(host=host, port=port)
