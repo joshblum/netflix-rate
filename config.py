1d91c25b784bb3585a8c9fe5a487ac83f31e8d55
Joshua Blum <joshblum@mit.edu>
2014-07-20 09:30:34 -0400

    unused

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
     SEND_FILE_MAX_AGE_DEFAULT = 60
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
